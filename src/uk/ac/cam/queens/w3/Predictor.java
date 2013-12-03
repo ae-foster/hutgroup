3d8bfffaf765660ac392b19c970d7e4f8fff4f49
JeppeHallgren <jeppe@hallgren.com>
2013-12-03 19:31:35 +0000

    Main class

---
+++ src/uk/ac/cam/queens/w3/Predictor.java
@@ -0,0 +1,14 @@
+package uk.ac.cam.queens.w3;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jh
+ * Date: 12/3/13
+ * Time: 7:28 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Predictor {
+    public static void main (String[] args){
+        System.out.println("test");
+    }
+}
